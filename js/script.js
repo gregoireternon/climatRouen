ebf64bab37cac59003907723771336eedc3352f3
Gregoire Ternon <gregoire.ternon@mail.com>
2019-01-03 19:35:30 +0100

    ajout d'une nouvelle page

--- js/script.js
+++ js/script.js
@@ -3,4 +3,11 @@ $(document).ready(function(){
     $('#global').change( function(){
         console.log('resized');
     });
+    $('#evenementLink').on('click',function(){
+        $.ajax({
+            url: 'evenements.html'
+         }).done(function(d){
+             alert(JSON.stringify(d));
+         });
+    });
 });
