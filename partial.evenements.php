e7385a83b140d39126384f93fb84e1f21125ab05
Gregoire Ternon <gregoire.ternon@mail.com>
2019-02-10 16:18:49 +0100

    le rouge et le vert

--- partial.evenements.php
+++ partial.evenements.php
@@ -1,11 +1,13 @@
 <div id="etage1" class="etage">
     <div class="textBlock">
-        <h2>Marche du soir pour le climat!!</h2>
-        <p>Notre action pour le weekend action climat est d&eacute;cid&eacute;e, et ce sera une marche en soir&eacute;e, le vendredi 25 &agrave; partir de 18h, au d&eacute;part de la place de la cath&eacute;drale &agrave; Rouen. Venez nombreux!</p>
-        <a href="https://www.facebook.com/events/2272621032790860/"><p>Les d&eacute;tails l&apos;&eacute;v&eacute;n&eacute;ment sur facebook</p></a>
+        <h2>Pi&egrave;ce de th&eacute;atre: le rouge et le vert</h2>
+        <p>Dialogue entre un &eacute;cologiste et un syndicaliste</p>
+        <p>Samedi 16 mars 2019, 20h</p>
+        <p>Th&eacute;atre de l&apos;echo du Robec</p>
+        
     </div>
-    <div class="imgBlock" style="min-width: 20rem;min-height: 16.6rem;">
-        <a href="https://www.facebook.com/events/2272621032790860/"><img src="img/weActionClimat.jpg"></a>
+    <div class="imgBlock" style="min-width: 20rem;min-height: 45.6rem;">
+        <img src="img/16m.png">
     </div>
 </div>
 <div id="etage2" class="etage">
