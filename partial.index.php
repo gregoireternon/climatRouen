8fbde7f765305c3dc4a206e7adbb996d2b74b015
Gregoire Ternon <gregoire.ternon@mail.com>
2019-09-05 21:56:09 +0200

    marche du 21... attente

--- partial.index.php
+++ partial.index.php
@@ -1,13 +1,15 @@
 <div id="intro2" class="etage">
     <div class="textBlock">
-        <p class="mirageEnd">Nouvelle marche organis&eacute;e &agrave; Rouen le 25 mai!!! <a href="https://www.facebook.com/events/268633880571764/" >Détails ici</a></p>
-        <h2 class="mirage">Nouvelle marche organis&eacute;e &agrave; Rouen le 25 mai!!!</h2>
-        <p class="mirage">C&apos;est reparti: une nouvelle occasion de montrer notre d&eacute;termination dans un contexte familial, ouvert, non violent et festif.</p>
-        <p class="mirage">Venez avec des pancartes, de quoi faire du bruit ou m&ecirc;me d&eacute;guis&eacute;s.</p>
-        <p class="mirage">Plus de d&eacute;tails  <a href="https://www.facebook.com/events/268633880571764/" >ici</a></p>
+        <h2>Nouvelle marche organis&eacute;e &agrave; Rouen le samedi 21 septembre!!!</h2>
+        <p>Une nouvelle marche est en préparation pour le samedi 21 septembre.</p>
+        <p>Départ à 10h30 devant la gare de Rouen</p>
+        <p>Arrivée à midi, friche Lucien</p>
+        <p>Plus de détails bientôt, mais vous pouvez d'ores et déjà réserver votre date!!!</p>
     </div>
-    <div class="imgBlock mirage" style="min-width: 20rem;min-height: 45.6rem;">
-        <img src="img/marche25mai.jpg" border: grey 1px solid;/>
+</div>
+<div id="intro2" class="etage">
+    <div class="imgBlock" style="min-width: 20rem;min-height: 45.6rem;">
+        <img src="img/received_447526686086941.jpeg" border: grey 1px solid;/>
     </div>
 </div>
 <div id="boxes">
@@ -15,26 +17,3 @@
     <div class="box liensLink" id="liensLink2"><a href="liens.php"><img src="img/links.PNG"  class="button"></a></div>
     <div class="box"><a href="https://www.facebook.com/groups/271172083511190/"><img src="img/facebook.PNG"  class="button"></a></div>
 </div>
-<div id="etage1" class="etage">
-    <div class="textBlock">
-        <p>Le grand mouvement de la transition &eacute;cologique est d&eacute;sormais lanc&eacute; !</p>
-        <p>Et la bonne nouvelle c&rsquor;est que vous aussi, vous pouvez participer &agrave; cette aventure,</p>
-        <p>Vous,</p>
-        <p>nous,</p>
-        <p>le collectif des Citoyens pour le Climat, tous engag&eacute;s vers la reconqu&ecirc;te d&rsquor;un monde plus humain</p>
-        <p>Ensemble, nous allons d&eacute;placer des montagnes !</p>
-        <p><i>&laquo; Rien n&rsquor;arr&ecirc;te la force d&rsquor;une id&eacute;e quand son heure est venue &raquo;</i> Victor Hugo</p>
-    </div>
-    <div class="imgBlock" style="min-width: 20rem;min-height: 11.2rem;">
-        <img src="img/friday.JPG">
-    </div>
-    <div class="textBlock">
-        <h2>Que faire ?</h2>
-        <p>Le climat se r&eacute;chauffe et notre belle plan&egrave;te bleue suffoque sous les d&eacute;chets, l&rsquor;humanit&eacute; est gravement en danger, mais que faire ?</p>
-        <h3>RESISTER !</h3>
-        <p>R&eacute;sister, c&rsquor;est penser autrement, rire, faire confiance, ETRE plut&ocirc;t qu&rsquor;AVOIR</p>
-        <p>R&eacute;sister, c&rsquor;est laisser passer le vent, la lumi&egrave;re, s&rsquor;accorder du temps, &ecirc;tre ensemble, se sentir libre</p>
-        <p>R&eacute;sister, c&rsquor;est reprendre le contr&ocirc;le de notre existence</p>
-    </div>
-</div>
-
