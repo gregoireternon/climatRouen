f79e14a378a3e182f75fdf1a55df0b54ab59ab70
Gregoire Ternon <gregoire.ternon@mail.com>
2019-09-08 18:32:07 +0200

    Avec Affiche de Julie

--- partial.index.php
+++ partial.index.php
@@ -5,22 +5,17 @@
         <p>Départ à 10h30 devant la gare de Rouen</p>
         <p>Arrivée à midi, friche Lucien</p>
         <p>Plus de détails bientôt, mais vous pouvez d'ores et déjà réserver votre date!!!</p>
-    </div>
-</div>
-<div id="intro2" class="etage">
-    <div class="textBlock">
         <h2>Cette marche est organisée par la coalition climat</h2>
         <p>La coalition  climat est un regroupement
             d'associations, de collectifs citoyens et
             d'organisations syndicales</p>
         <p>Retrouvez les associations qui la constituent dans le document <a href="doc/FlyerCoalition.pdf">accessible ici.</a> Vous trouverez aussi les coordonnées de chacune d'elles.</p>
     </div>
-</div>
-<div id="intro2" class="etage">
     <div class="imgBlock" style="min-width: 20rem;min-height: 45.6rem;">
-        <img src="img/received_447526686086941.jpeg" border: grey 1px solid;/>
+        <img src="img/marche21.jpg" border: grey 1px solid;/>
     </div>
 </div>
+
 <div id="boxes">
     <div class="box evenementLink" id="evenementLink2"><a href="evenements.php"><img src="img/events.PNG" class="button"></a></div>
     <div class="box liensLink" id="liensLink2"><a href="liens.php"><img src="img/links.PNG"  class="button"></a></div>
