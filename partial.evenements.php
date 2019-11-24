415265f63a99ab5c7a44fc3f0fb41f0a7a7a9120
Gregoire Ternon <gregoire.ternon@mail.com>
2019-11-24 21:00:43 +0100

    marche 11 2019

--- partial.evenements.php
+++ partial.evenements.php
@@ -1,12 +1,19 @@
 <div id="intro2" class="etage">
     <div class="textBlock">
-        <h2 ><a href="https://www.facebook.com/events/268633880571764/" >Nouvelle marche organis&eacute;e &agrave; Rouen le 25 mai!!!</a></h2>
-        <p >C&apos;est reparti: une nouvelle occasion de montrer notre d&eacute;termination dans un contexte familial, ouvert, non violent et festif.</p>
-        <p >Venez avec des pancartes, de quoi faire du bruit ou m&ecirc;me d&eacute;guis&eacute;s.</p>
-        <p >Plus de d&eacute;tails  <a href="https://www.facebook.com/events/268633880571764/" >ici</a></p>
+        <h2>Nouvelle marche organis&eacute;e &agrave; Rouen le samedi 30 novembre!!!</h2>
+        <p>Une nouvelle marche est en préparation pour le samedi 30 novembre.</p>
+        <p>Départ à 10h30 devant l'église St Sever</p>
+        
+        <p>L'événement facebook est ici: <a href="https://www.facebook.com/events/2140419159586637/">https://www.facebook.com/events/2140419159586637/</a> </p>
+        <h2>Cette marche est organisée par la coalition climat</h2>
+        <p>La coalition  climat est un regroupement
+            d'associations, de collectifs citoyens et
+            d'organisations syndicales</p>
+        <p>Retrouvez les associations qui la constituent dans le document <a href="doc/FlyerCoalition.pdf">accessible ici.</a> Vous trouverez aussi les coordonnées de chacune d'elles.</p>
+        
     </div>
-    <div class="imgBlock " style="min-width: 20rem;min-height: 45.6rem;">
-        <img src="img/marche25mai.jpg" style="border: grey 1px solid;"/>
+    <div class="imgBlock" style="min-width: 20rem;min-height: 45.6rem;">
+        <img src="img/affiche_finale_HD.png" border: grey 1px solid;/>
     </div>
 </div>
 <div id="etage1" class="etage">
@@ -24,13 +31,4 @@
         <a href="https://www.facebook.com/events/2272621032790860/"><p>Les d&eacute;tails l&apos;&eacute;v&eacute;n&eacute;ment sur facebook</p></a>
     </div>
     <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/MnZRLoHyseo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-</div>
-<div id="etage2" class="etage">
-    <div class="textBlock">
-        <h2>Cette ann&eacute;e a lieu l&apos;Armada &agrave; Rouen</h2>
-        <p>Des id&eacute;es pour une action? Faites nous en part!</p>
-    </div>
-    <div class="imgBlock" style="min-width: 20rem;min-height: 16.6rem;">
-        <img src="http://www.armada.org/assets/images/logo.png">
-    </div>
 </div>
--- partial.index.php
+++ partial.index.php
@@ -1,27 +1,10 @@
 <div id="intro2" class="etage">
     <div class="textBlock">
-        <h2>Nouvelle marche organis&eacute;e &agrave; Rouen le samedi 21 septembre!!!</h2>
-        <p>Une nouvelle marche est en préparation pour le samedi 21 septembre.</p>
-        <p>Départ à 10h30 devant la gare de Rouen</p>
-        <p>Puis:
-        <ul>
-            <li>rue Jeanne d'Arc</li>
-            <li>rue Jean Lecanuet</li>
-            <li>rue Beauvoisine</li>
-            <li>rue de l'Hôpital</li>
-            <li>rue de la République</li>
-            <li>rue d'Amiens</li>
-            <li>rue Armand Carrel</li>
-            <li>rue de Martainville</li>
-            <li>rue Victor Hugo</li>
-            <li>rue Alsace Lorraine (voies TEOR)</li>
-            <li>rue Grand Pont</li>
-            <li>pont Boieldieu</li>
-            <li>cours Clémenceau</li>
-            <li>place Carnot</li>
-        </ul> </p>
-        <p>Arrivée à midi, friche Lucien, dans le cadre du Village Alternatiba</p>
-        <p>L'événement facebook est ici: <a href="https://www.facebook.com/events/651992861962787/">https://www.facebook.com/events/651992861962787/</a> </p>
+        <h2>Nouvelle marche organis&eacute;e &agrave; Rouen le samedi 30 novembre!!!</h2>
+        <p>Une nouvelle marche est en préparation pour le samedi 30 novembre.</p>
+        <p>Départ à 10h30 devant l'église St Sever</p>
+        
+        <p>L'événement facebook est ici: <a href="https://www.facebook.com/events/2140419159586637/">https://www.facebook.com/events/2140419159586637/</a> </p>
         <h2>Cette marche est organisée par la coalition climat</h2>
         <p>La coalition  climat est un regroupement
             d'associations, de collectifs citoyens et
@@ -30,7 +13,7 @@
         
     </div>
     <div class="imgBlock" style="min-width: 20rem;min-height: 45.6rem;">
-        <img src="img/marche21.jpg" border: grey 1px solid;/>
+        <img src="img/affiche_finale_HD.png" border: grey 1px solid;/>
     </div>
 </div>
 
